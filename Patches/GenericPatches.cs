e1b5e9b4152bbf9b4601447fbf95561e054218ba
Pietro420 <77608535+Pietrodjaowjao@users.noreply.github.com>
2026-04-03 15:39:23 -0300

    fix player particles

--- Patches/GenericPatches.cs
+++ Patches/GenericPatches.cs
@@ -621,3 +621,41 @@ public static class ChangeVoteAreaFont
         __instance.NameText.ForceMeshUpdate();
     }
 }
+
+[HarmonyPatch(typeof(PlayerParticles), nameof(PlayerParticles.Start))]
+public static class FixNoParticle
+{
+    // Handle scenario where there's no april fools sprite.
+
+    public static bool Prefix(PlayerParticles __instance)
+    {
+        __instance.fill = new RandomFill<PlayerParticleInfo>();
+        if (AprilFoolsMode.ShouldHorseAround() && __instance.HorseSprites != null && __instance.HorseSprites.Length > 0)
+        {
+            __instance.fill.Set(__instance.HorseSprites.TryCast<Il2CppSystem.Collections.Generic.IEnumerable<PlayerParticleInfo>>());
+            __instance.pool.Prefab = __instance.HorsePrefab;
+            __instance.scaleModifier = 0.7f;
+        }
+        else if (AprilFoolsMode.ShouldClassicMainMenuMode() && __instance.ClassicSprites != null && __instance.ClassicSprites.Length > 0)
+        {
+            __instance.fill.Set(__instance.ClassicSprites.TryCast<Il2CppSystem.Collections.Generic.IEnumerable<PlayerParticleInfo>>());
+            __instance.scaleModifier = 4f;
+        }
+        else
+        {
+            __instance.fill.Set(__instance.Sprites.TryCast<Il2CppSystem.Collections.Generic.IEnumerable<PlayerParticleInfo>>());
+            __instance.scaleModifier = 1f;
+        }
+        int num = 0;
+        if (__instance.pool != null)
+        {
+            while (__instance.pool.NotInUse > 0)
+            {
+                PlayerParticle playerParticle = __instance.pool.Get<PlayerParticle>();
+                PlayerMaterial.SetColors(num++, playerParticle.myRend);
+                __instance.PlacePlayer(playerParticle, true);
+            }
+        }
+        return false;
+    }
+}
